ca2fc6f8d1b47e25d8e5258b27ec5d0f1984addb
tiipaa <bababoost@hotmail.com>
2025-11-03 21:06:06 +0100

    ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base : "/image-sizer"
+  base : "/image-sizer",
   build: {
     outDir: 'dist',
     sourcemap: true,
